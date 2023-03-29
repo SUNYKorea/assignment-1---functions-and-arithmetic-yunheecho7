3f0f8c118bae9904b59fc5a3024ae0990e06092d
Yunhee Cho <yunheecho7@gmail.com>
2023-03-29 21:38:51 +0900

    I remodified exercises 1, 2, and 3

--- Homework_1.py
+++ Homework_1.py
@@ -14,24 +14,27 @@ def fahrenheit2celsius (fahrenheit): #user input function
 
 def what_to_wear(celsius):
 
-    if celsius < -10:
+    if celsius < -10: #if the given celsius is less than -10 degrees, then the user needs to wear a puffy jacket
         return "Puffy jacket"
-    elif -10 <= celsius <= 0:
+    elif -10 <= celsius <= 0: #...less than or equal to 0 degrees,...a scarf
         return "Scarf"
-    elif 0 <= celsius <= 10:
+    elif 0 <= celsius <= 10: #...less than or equal to 10 degrees,...a sweater
         return "Sweater"
-    elif 10 <= celsius <= 20:
+    elif 10 <= celsius <= 20: #...less than or equal to 20 degrees,...a light jacket
         return "Light Jacket"
-    elif celsius > 20:
+    elif celsius > 20: #...greater than 20 degrees,...a t-shirt
         return "T-shirt"
     
-#Calling functions fahrenheit2celsius and what_to_wear (this is where I got stuck the most for this exercise because I didn't know how to call them)
+
 
 fahrenheit = float(input("Enter the temperature in fahrenheit: "))
+
+#Calling functions fahrenheit2celsius and what_to_wear (this is where I got stuck the most for this exercise because I didn't know how to call them)
 celsius = fahrenheit2celsius(fahrenheit)
 clothing = what_to_wear(celsius)
-print("The temperature in celsius is: ", str(celsius))
-print("You need to wear a: ", clothing)
+
+print("The temperature in celsius is: ", str(celsius)) #prints out the temperature after converting from fahrenheit
+print("You need to wear a: ", clothing) #prints out the clothing the user needs to wear given the temperature in celsius 
 
 #This function is fruitful because it returns a value, which is the type of clothing in this case.
 
@@ -42,21 +45,16 @@ print("You need to wear a: ", clothing)
 # compute_triangle_perimeter from scratch  
 
 def shoelace_triangle_area(x1, y1, x2, y2, x3, y3):
-
-    area = abs((x1 * y2 + x2 * y3 + x3 * y1) - (x1 * y3 + x2 * y1 + x3 * y2)) / 2 #Formula for solving area of a triangle
-    return area 
+    A = abs((x1 * y2 + x2 * y3 + x3 * y1) - (x1 * y3 + x2 * y1 + x3 * y2)) / 2 #Formula for solving area of a triangle
+    return A
 
 
 def euclidean_distance(x1, y1, x2, y2):
-   
-    euclidean_distance = (((x1 - x2)) ** 2 + (y1 - y2) ** 2) ** 0.5 #Formula for the euclidean distance
-    return euclidean_distance
+    d = (((x1 - x2)) ** 2 + (y1 - y2) ** 2) ** 0.5 #Formula for the euclidean distance
+    return d
+    
 
 def compute_triangle_perimeter(x1, y1, x2, y2, x3, y3):
-    s1 = euclidean_distance(x1, y1, x2, y2) #The distance obtained from x1, y1, x2, y2 after using the euclidean distance formula
-    s2 = euclidean_distance(x2, y2, x3, y3) #...from x2, y2, x3, y3 ...
-    s3 = euclidean_distance(x3, y3, x1, y1) #...from x3, y3, x1, y1 ...
-
     P = s1 + s2 + s3 #The formula for the perimeter of a triangle
     return P
 
@@ -67,37 +65,38 @@ y1 = int(input("y1: "))
 y2 = int(input("y2: "))
 y3 = int(input("y3: "))
 
-s1 = euclidean_distance(x1, y1, x2, y2) 
-s2 = euclidean_distance(x2, y2, x3, y3) 
-s3 = euclidean_distance(x3, y3, x1, y1)
+s1 = euclidean_distance(x1, y1, x2, y2) #The distance obtained from x1, y1, x2, y2 using the euclidean distance formula
+s2 = euclidean_distance(x2, y2, x3, y3) #...from x2, y2, x3, y3 ...
+s3 = euclidean_distance(x3, y3, x1, y1) #...from x3, y3, x1, y1 ...
 
+#Calling functions compute_triangle_perimeter and shoelace_triangle_area 
 P = compute_triangle_perimeter(x1, y1, x2, y2, x3, y3)
-area = shoelace_triangle_area(x1, y1, x2, y2, x3, y3)
-print("The perimeter of a triangle is: ", str(P))
-print("The area of a triangle is: " , str(area))
+A = shoelace_triangle_area(x1, y1, x2, y2, x3, y3)
+
+print("The perimeter of a triangle is: ", str(P)) #prints out the perimeter of a triangle
+print("The area of a triangle is: " , str(A)) #prints out the area of a triangle
 
 # ---------------------------- Exercise III -------------------------------------
 # ----------------- Compute the area of a regular polygon -----------------------
 # TODO: Fill the functions deg2rad, apothem  and polygon_area 
 
+#import math module for tan and pi
 from math import tan, pi
 
 def deg2rad(deg):
-    return deg * (pi / 180)
+    return deg * (pi / 180) #conversion from degrees to radians
 
-def apothem(number_sides, length_side):
-   
-    return a
+def apothem(number_sides, length_side): 
+    return a 
 
 def polygon_area(number_sides, length_side):
-    
     return area
 
-number_sides = int(input("Enter the side: "))
-length_side = int(input("Enter the length side "))
-a = length_side / (2 * tan(pi / number_sides))
-area = (number_sides * length_side * a) / 2
-print("The area of the regular polygon is: ", str(area))
+number_sides = int(input("Enter the side: ")) #user inputs the value for the side
+length_side = int(input("Enter the length side ")) #user inputs the value for the side of a length
+a = length_side / (2 * tan(pi / number_sides)) # formula for the apothem, a,  defined in def polygon area
+area = (number_sides * length_side * a) / 2 #formula for the area of a regular polygon,defined in the function def polygon_area
+print("The area of the regular polygon is: ", str(area)) #prints out the area of a polygon after inputing side length and side
 
 # ---------------------------- Test -------------------------------------
 # The following lines are for testing purposes, and will not be part of
